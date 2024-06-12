410165788f932d4abd3d571acc0ed5210d78d927
Bohra <bohrac@wit.edu>
2024-06-11 23:23:40 -0400

    commiting backend

---
+++ e-comBackend/src/main/java/com/chirag/e_comBackend/EComBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chirag.e_comBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComBackendApplication.class, args);
+	}
+
+}
